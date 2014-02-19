e59024d0fe8b0532755e95b39cc77bb9248868b7
agreenville <aaron.greenville@sydney.edu.au>
2014-02-19 12:23:29 +1100

    more ideas for analysis

--- analysis.R
+++ analysis.R
@@ -1,5 +1,8 @@
 data <- read.csv("data/gapminder-FiveYearData.csv", stringsAsFactors=FALSE)
 
+#prob need some data to subset 1st
+
 #subset data.
 #this will be version control
 
+#this will be controlled by git
